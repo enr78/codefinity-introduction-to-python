1598a1b91496a03145b8ce5b3f26e6cd70ab8ad1
enr78 <>
2025-12-16 01:49:00 +0000

    Tuple Operations

---
+++ other_data_types/tuple_operations/main.py
@@ -0,0 +1,5 @@
+# Initial items on shelf #1 (provided as a tuple)
+shelf1 = ("celery", "spinach", "cucumbers")
+
+# Items being added to the shelf #1 (provided as a list)
+shelf1_update = ["tomatoes", "celery", "cilantro"]
