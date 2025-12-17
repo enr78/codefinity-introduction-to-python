2b6a66bac3cdf75626423729b194430dae04d5f4
enr78 <>
2025-12-17 04:30:01 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,35 @@
+# prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+
+# Write your code here
+
+
+
+
+
+# # below is just from the video except for dictionary. 
+# # dictionary is my guess
+# # for "variable" in sequence:
+# # list
+# dairy_products = ["Milk", "Cheese", "Yogurt", "Custard", "Pudding"]
+# # tuple 
+# types_of_bread = ("Sourdough", "Whole Wheat", "Brioche", "Rye", "Multigrain")
+# # dictionary
+# # mens_hats = {"Fedora": 6, "Beanie": 24, "Baseball Cap": 30}
+# # for stock in mens_hats:
+# #     print(stock)
+# # for stock in mens_hats.values():
+# #     print(stock)
+
+# # variable
+# favorite_fruit = "Apple"
+# # loop
+# for product in dairy_products:
+#     print(product)
+
+# for letter in favorite_fruit:
+#     print(letter)
+
+
+
+
+
