e8c6a4f08439211303b1977c1c5e155de55ecd0b
enr78 <>
2026-01-07 22:43:42 +0000

    For Loops

--- loops/for_loops/main.py
+++ loops/for_loops/main.py
@@ -1,35 +1,11 @@
-# prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
 
 # Write your code here
 
+total = 0
 
-
-
-
-# # below is just from the video except for dictionary. 
-# # dictionary is my guess
-# # for "variable" in sequence:
-# # list
-# dairy_products = ["Milk", "Cheese", "Yogurt", "Custard", "Pudding"]
-# # tuple 
-# types_of_bread = ("Sourdough", "Whole Wheat", "Brioche", "Rye", "Multigrain")
-# # dictionary
-# # mens_hats = {"Fedora": 6, "Beanie": 24, "Baseball Cap": 30}
-# # for stock in mens_hats:
-# #     print(stock)
-# # for stock in mens_hats.values():
-# #     print(stock)
-
-# # variable
-# favorite_fruit = "Apple"
-# # loop
-# for product in dairy_products:
-#     print(product)
-
-# for letter in favorite_fruit:
-#     print(letter)
-
-
-
-
-
+# iterate through prices adding each item to the looping total 
+for price in prices:
+    total += price
+# print last iteration of total 
+print(total)
